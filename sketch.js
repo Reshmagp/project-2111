088fa5fd1ae5037f932e7e3bcae76392769c2af4
SharayuPawar <76246278+SharayuPawar@users.noreply.github.com>
2021-01-20 11:35:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var tom,tomImage;
+var jery,jeryImage;
+var ground;
+
+
+
+function preload() {
+tomImage=loadImage('tomTwo.png');
+    //load the images here
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+tom=createSprite(800,550,20,20);
+tom.addImage("tomImage");
+ground=createSprite(50,600,10000,10);
+jerry=createSprite(100,550,20,20);
+}
+
+function draw() {
+
+    background(225);
+    
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
